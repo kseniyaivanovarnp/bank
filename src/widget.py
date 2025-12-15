f2dbe9fd0f63cfac38db7d29efdd2f25adc11fe5
Ксения Тимошенкова <kseniyaivanovarnp@gmail.com>
2025-12-15 19:42:19 +0300

    Добавлена функция get_date в widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def mask_account_card(info: str) -> str:
     """Разделяем строку на тип и номер по последнему пробелу"""
     type_and_number = info.rsplit(" ", 1)
@@ -14,3 +17,10 @@ def mask_account_card(info: str) -> str:
 
     """ Возврат замаскированной строки """
     return f"{type_} {masked_number}"
+
+
+def get_date(date_string: str) -> str:
+    """Преобразуем строку в объект datetime"""
+    date_object = datetime.strptime(date_string, "%Y-%m-%dT%H:%M:%S.%f")
+    """ Форматируем объект datetime в строку нужного формата """
+    return date_object.strftime("%d.%m.%Y")
